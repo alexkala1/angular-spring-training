dc31435cba3917e3e75147916f746f08e2a61705
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2021-06-23 17:06:27 +0300

    Implement view child and view children examples

--- training-project-front/src/app/home/home.component.ts
+++ training-project-front/src/app/home/home.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { CarCardComponent } from '../components/car-card/car-card.component';
 import { Car } from '../interface/car';
 import { HomeService } from './home.service';
 
@@ -9,12 +16,30 @@ import { HomeService } from './home.service';
 })
 export class HomeComponent implements OnInit {
   cars: any;
+  discountCodeApplied: boolean = false;
+
+  @ViewChild(CarCardComponent) child!: CarCardComponent;
+  @ViewChildren(CarCardComponent) children!: QueryList<CarCardComponent>;
+
   constructor(private homeService: HomeService) {}
 
   ngOnInit(): void {
-		this.cars = []
+    this.cars = [];
     this.homeService
       .getAllCars()
       .subscribe((response) => (this.cars = response), console.error);
+  }
+
+  addToCart(car: Car) {
+    this.child?.addToCart(car);
+  }
+
+  applyDiscountCode(): void {
+    this.children.forEach((child) => {
+			if (child.car.mileage && !this.discountCodeApplied)
+				child.car.mileage /= 2
+		});
+
+		this.discountCodeApplied = true
   }
 }
